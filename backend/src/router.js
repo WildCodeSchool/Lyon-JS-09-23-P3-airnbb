083b9a065c398f532df700ad60d97c99657cde54
Cricri Au <andcricri93@gmail.com>
2023-12-15 16:12:04 +0100

    us2_createGetAllRequest avec modification de l'arborescence

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,22 +2,30 @@ const express = require("express");
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
-
 // Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
+const getAllParent = require("./controllers/parentController");
 
-// Route to get a list of items
-router.get("/items", itemControllers.browse);
-
-// Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
-
-// Route to add a new item
-router.post("/items", itemControllers.add);
-
-/* ************************************************************************* */
+// Route to get a list of parent
+router.get("/parent", getAllParent);
 
 module.exports = router;
+
+// /* ************************************************************************* */
+// // Define Your API Routes Here
+// /* ************************************************************************* */
+
+// // Import itemControllers module for handling item-related operations
+// const itemControllers = require("./controllers/itemControllers");
+
+// // Route to get a list of items
+// router.get("/items", itemControllers.browse);
+
+// // Route to get a specific item by ID
+// router.get("/items/:id", itemControllers.read);
+
+// // Route to add a new item
+// router.post("/items", itemControllers.add);
+
+// /* ************************************************************************* */
+
+// module.exports = router;
