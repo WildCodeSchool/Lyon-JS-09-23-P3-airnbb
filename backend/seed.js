88d5bdd2b1719a0d68730fbd808d184508565a2f
Cricri Au <andcricri93@gmail.com>
2023-12-20 12:16:58 +0100

    fix double comments

--- backend/seed.js
+++ backend/seed.js
@@ -1,49 +1,49 @@
-// /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 
-// // Load environment variables from .env file
-// require("dotenv").config();
+// Load environment variables from .env file
+require("dotenv").config();
 
-// // Import Faker library for generating fake data
-// const { faker } = require("@faker-js/faker");
+// Import Faker library for generating fake data
+const { faker } = require("@faker-js/faker");
 
-// // Import database client
-// const database = require("./database/client");
+// Import database client
+const database = require("./database/client");
 
-// const seed = async () => {
-//   try {
-//     // Declare an array to store the query promises
-//     // See why here: https://eslint.org/docs/latest/rules/no-await-in-loop
-//     const queries = [];
+const seed = async () => {
+  try {
+    // Declare an array to store the query promises
+    // See why here: https://eslint.org/docs/latest/rules/no-await-in-loop
+    const queries = [];
 
-//     /* ************************************************************************* */
+    /* ************************************************************************* */
 
-//     // Generating Seed Data
+    // Generating Seed Data
 
-//     // Optional: Truncate tables (remove existing data)
-//     await database.query("truncate item");
+    // Optional: Truncate tables (remove existing data)
+    await database.query("truncate item");
 
-//     // Insert fake data into the 'item' table
-//     for (let i = 0; i < 10; i += 1) {
-//       queries.push(
-//         database.query("insert into item(title) values (?)", [
-//           faker.lorem.word(),
-//         ])
-//       );
-//     }
+    // Insert fake data into the 'item' table
+    for (let i = 0; i < 10; i += 1) {
+      queries.push(
+        database.query("insert into item(title) values (?)", [
+          faker.lorem.word(),
+        ])
+      );
+    }
 
-//     /* ************************************************************************* */
+    /* ************************************************************************* */
 
-//     // Wait for all the insertion queries to complete
-//     await Promise.all(queries);
+    // Wait for all the insertion queries to complete
+    await Promise.all(queries);
 
-//     // Close the database connection
-//     database.end();
+    // Close the database connection
+    database.end();
 
-//     console.info(`${database.databaseName} filled from ${__filename} 🌱`);
-//   } catch (err) {
-//     console.error("Error filling the database:", err.message);
-//   }
-// };
+    console.info(`${database.databaseName} filled from ${__filename} 🌱`);
+  } catch (err) {
+    console.error("Error filling the database:", err.message);
+  }
+};
 
-// // Run the seed function
-// seed();
+// Run the seed function
+seed();
