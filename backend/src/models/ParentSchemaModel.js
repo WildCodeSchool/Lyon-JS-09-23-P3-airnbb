c7b0416393dbb9aae5f462ba3cec287c5e9758bd
Sharmake Awil <sharmarke.awil@hotmail.com>
2023-12-20 11:36:50 +0100

    fix comentaire dans Parent Schema model

--- backend/src/models/ParentSchemaModel.js
+++ backend/src/models/ParentSchemaModel.js
@@ -7,6 +7,6 @@ const parentPostSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   address: { type: String, required: true },
 });
-
+/** ici on a mis deux nom parent parce que le premier c'est le schema et le deuxieme c'est le nom de la collection */
 const Parent = mongoose.model("parent", parentPostSchema, "parent");
 module.exports = Parent;
