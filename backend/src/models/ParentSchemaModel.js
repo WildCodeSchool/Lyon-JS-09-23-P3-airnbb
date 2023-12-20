f61c242535d2d2f009eb1ac08e591b5e47bff81d
Sharmake Awil <sharmarke.awil@hotmail.com>
2023-12-20 12:07:40 +0100

    commentaire modifié

--- backend/src/models/ParentSchemaModel.js
+++ backend/src/models/ParentSchemaModel.js
@@ -7,6 +7,6 @@ const parentPostSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   address: { type: String, required: true },
 });
-
+/* ici on a trois parametre 1 c'est nom du model, 2 nom du schema et 3 c'est le nom de la collection */
 const Parent = mongoose.model("parent", parentPostSchema, "parent");
 module.exports = Parent;
