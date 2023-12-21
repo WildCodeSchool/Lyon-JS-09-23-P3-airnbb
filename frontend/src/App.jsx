6ba74cc74be8b43f1899253e7b9e40290e8388a2
Alexis_Lienard <lienard.alexis@gmail.com>
2023-12-21 16:32:21 +0100

    + added conditionnal routing depending on screensize

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,25 +9,35 @@ import {
 import IntroLayout from "./layouts/IntroLayout";
 
 // pages
-import Intro from "./pages/intro/Intro";
-import Signup from "./pages/intro/Signup";
 import Login from "./pages/intro/Login";
+import Signup from "./pages/intro/Signup";
+import Intro from "./pages/intro/Intro";
 
 // hooks
+import useScreenSize from "./hooks/useScreenSize";
 
 // style
 import "./App.css";
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
+  const screenSize = useScreenSize();
+
+  const routes =
+    screenSize.width > 705 ? (
+      <Route path="/" element={<IntroLayout />}>
+        <Route path="/signup" element={<Signup />} />
+        <Route index element={<Login />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    ) : (
       <Route path="/" element={<IntroLayout />}>
         <Route index element={<Intro />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Route>
-    )
-  );
+    );
+
+  const router = createBrowserRouter(createRoutesFromElements(routes));
 
   return <RouterProvider router={router} />;
 }
--- frontend/src/layouts/IntroLayout.jsx
+++ frontend/src/layouts/IntroLayout.jsx
@@ -1,12 +1,26 @@
 // components
 import { Outlet } from "react-router-dom";
 
+// pages
+import Intro from "../pages/intro/Intro";
+
+// hooks
+import useScreenSize from "../hooks/useScreenSize";
+
 // styles
 import "./styles/IntroLayout.css";
 
 function IntroLayout() {
+  const screenSize = useScreenSize();
+
   return (
     <div className="container">
+      {screenSize.width > 705 && (
+        <section className="introLayout">
+          <Intro screenSize={screenSize} />
+        </section>
+      )}
+
       <section className="introLayout">
         <Outlet />
       </section>
