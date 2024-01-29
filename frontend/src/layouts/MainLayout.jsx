4de15452af009bcd034e8757c8206bf22f848f17
Messaoud ZOUGGAB <messaoud.zouggab@gmail.com>
2024-01-29 11:13:59 +0100

    S5_US10_adaptationNavbarFromMobileToDesktop done

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,13 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import NavbarDesktop from "../components/Navbar/NavbarDesktop";
+
+// hooks
+import useScreenSize from "../hooks/useScreenSize";
 
 function MainLayout() {
-  return (
+  const screenSize = useScreenSize();
+
+  const nav = (
     <>
       <Outlet />
-      <Navbar />
+      {screenSize.width > 705 ? <NavbarDesktop /> : <Navbar />}
     </>
   );
+
+  return nav;
 }
 
 export default MainLayout;
