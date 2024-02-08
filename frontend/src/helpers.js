05163c7dac37f8a85aeb82aa1b74f2a15072f3e4
Alexis_Lienard <lienard.alexis@gmail.com>
2024-02-08 13:29:45 +0100

    issue #64 => #bugfix : children can no longer be older than 3 years old. You can't create an unborn child either

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -58,3 +58,15 @@ export const formatDate = (date) => {
 export const checkFieldsFilled = (arrayToCheck) => {
   return !arrayToCheck.some((value) => value === "");
 };
+
+// set maximal age a child can have
+export const maximalAge = () => {
+  const currentDate = new Date();
+
+  const threeYearsAgo = new Date(currentDate);
+  threeYearsAgo.setFullYear(currentDate.getFullYear() - 3);
+
+  const formattedDate = threeYearsAgo.toISOString().split("T")[0];
+
+  return formattedDate;
+};
